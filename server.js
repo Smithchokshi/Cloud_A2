a31653120a582a11e02366ff3dea296f88e87e90
Smith Chokshi <sm714486@dal.ca>
2023-06-10 23:47:24 -0300

    added cred for s3

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const path = require('path')
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
+const AWS = require('aws-sdk');
+
 
 const PORT = 50051;
 const protofile = './proto/computeandstorage.proto';
+
+const credentials = new AWS.Credentials({
+    accessKeyId: "ASIA6DHIQVMZQWF4NQNC",
+    secretAccessKey: "9bgFozthrThg/5lBR1ZkDD2x8thpxkFzc5xhfLrB",
+    sessionToken: "FwoGZXIvYXdzEHwaDBl/YpEf7ulBbwdVcyLAAXTazQVxOTsF6MfcMiHYPdA+Yu4ExaFRsfaWyEqyQB/ClLHRrchnm2ghxLHEWfyV/n35iLMOoJGRsSRhdSwTwDd3Q/pYu5F0B1hGt2a1mN7+lo7004NkfwlGStdGqgzmNzUkPTddd2XzPgxI5YwL3nMgjNDmBQ7NU3Yr01YE1cLKG8qwuvwmPHQqpBXG35XXXcDR2FIdIvPdxLFTViz6/JRXyL0RqzWxRBNFWzTgcYbHKKeX0tJ6neV14EA0XZgYTiip5pSkBjItSk4uACLGAdQAVFAai7CsPqNm+/NXlCOD9/HjyiIVQdpgOKAKqYsjWLKZF3Xn"
+})
+
+const s3 = new AWS.S3({ credentials });
+
 
 const packageDef = protoLoader.loadSync(path.resolve(__dirname, protofile), {
     keepCase: true,
@@ -35,7 +46,7 @@ function getServer() {
     const server = new grpc.Server();
     server.addService(computeandstorage.EC2Operations.service, {
         "StoreData": (req, res) => {
-            console.log(req, res);
+            console.log(req.request.data);
         },
         "AppendData": () => {
 
